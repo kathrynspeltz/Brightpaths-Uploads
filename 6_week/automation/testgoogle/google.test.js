f6937feb0497e914e277e641237ff7775ed63bd4
kathrynspeltz <kathrynspeltz@gmail.com>
2023-07-25 16:18:20 -0500

    Start of the week

---
+++ 6_week/automation/testgoogle/google.test.js
@@ -0,0 +1,40 @@
+const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
+
+let driver;
+
+beforeAll(async () => {
+  driver = new Builder().forBrowser(Browser.CHROME).build();
+});
+
+afterAll(async () => {
+  await driver.quit();
+});
+
+test("can search Google for 'automation'", async () => {
+  // Navigate to google.com
+  await driver.get("https://www.google.com/");
+  // TODO Navigate to google.com
+
+  // TODO Uncomment the line below and replace SEARCH_BAR_NAME with the name of the search bar element
+  await driver.findElement(By.name("q")).sendKeys("automation", Key.RETURN);
+
+  // Wait for the results page to load
+  await driver.wait(until.titleIs("automation - Google Search"), 1000000);
+});
+
+test("can search Google twice", async () => {
+  //   Fix the TODOs below to finish the test
+  //   TODO Navigate to google.com
+  await driver.get("https://www.google.com/");
+  //   TODO Search for something in Google and wait for the page to load
+  await driver.findElement(By.name("q")).sendKeys("must see travel destinations", Key.RETURN);
+  await driver.wait(until.titleIs("must see travel destinations - Google Search"), 1000000);
+  //   TODO Call .clear() on the search bar element to clear the old search term
+
+  await driver.findElement(By.name("q")).clear()
+  //   TODO Call .sendKeys() on the search bar element to search for a new term
+  //   TODO Wait for the results page to load
+  await driver.findElement(By.name("q")).sendKeys("cheapest flights from MSP", Key.RETURN);
+  await driver.wait(until.titleIs("cheapest flights from MSP - Google Search"), 1000000);
+
+});
